395e0686844a6acec85ba2cec44bda5e2f9c4863
sayainfotech <abhisheksaya98@gmail.com>
2023-03-12 16:55:06 -0400

    security rule update

---
+++ securityrule.tf
@@ -0,0 +1,13 @@
+resource "azurerm_network_security_rule" "tf-nsg-dev-rule" {
+  name                        = "tf-nsg-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.tf-rg.name
+  network_security_group_name = azurerm_network_security_group.tf-nsg.name
+}
