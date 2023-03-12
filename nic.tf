7917e07a652a20914497ce3a5769e84298cf5d0e
sayainfotech <abhisheksaya98@gmail.com>
2023-03-12 17:05:11 -0400

    nic created

---
+++ nic.tf
@@ -0,0 +1,16 @@
+resource "azurerm_network_interface" "tf-nic" {
+  name                = "tf-nic"
+  location            = azurerm_resource_group.tf-rg.location
+  resource_group_name = azurerm_resource_group.tf-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.tf-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.tf-nsg-pip.id
+  }
+ 
+ tags = {
+    environment = "dev"
+  }
+}
